bbe38e826193933f140334e1154026caf82f57dc
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-11 15:59:56 -0500

    Learned about constants
    Added some important learning comments by the end

--- experiments/review-new-js-experiments/js/script.js
+++ experiments/review-new-js-experiments/js/script.js
@@ -1,24 +1,39 @@
 /**
-Title of Project
+Constants
 Lam Ky Anh Do
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Experimenting with constants
+
+constants (const) basically turn variables into fixed variables that never change and cannot be change
 */
 
 "use strict";
 
+// const PI = 3.14159;
+// const I_LOVE_TO_LEARN = true;
+// const MY_FAVORITE_PROGRAMMING_LANGUAGE = `JavaScript`;
+// const HOW_MUCH_WOOD_COULD_A_WOODCHUCK_CHUCK = undefined;
 
-function preload() {
-
-}
-
+// Replace let numCircles = 10; by const
+const NUM_CIRCLES = 10;
+let circleAlpha = 50;
+let circleSizeIncrease = 50;
 
 function setup() {
-
+  createCanvas(500, 500);
 }
 
-
 function draw() {
+  background(0)
 
+  circleAlpha = map(mouseX, 0, width, 10, 100);
+  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+
+  // Apply const var to for loop
+  for (let i = 0; i < NUM_CIRCLES; i++) {
+    push();
+    fill(255, circleAlpha);
+    ellipse(width / 2, height / 2, i * circleSizeIncrease);
+    pop();
+  }
 }
