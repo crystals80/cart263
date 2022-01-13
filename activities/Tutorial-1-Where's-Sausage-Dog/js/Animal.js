afe0b056e5350e28de9c3bc63fe7b55edfee0b5f
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-12 22:22:40 -0500

    Improved code
    by changing mousePressed condition in child (SausageDog) class --- moving it to Animal class via an overlap function

--- activities/Tutorial-1-Where's-Sausage-Dog/js/Animal.js
+++ activities/Tutorial-1-Where's-Sausage-Dog/js/Animal.js
@@ -19,4 +19,14 @@ class Animal {
     image(this.image, 0, 0);
     pop();
   }
+
+  overlap(x, y) {
+    // Check whether the provided coordinates are inside the animal’s image dimensions and returns true if they are. OTHERWISE false
+    // Taken from child class (SausageDog) but replacing mouseX/Y by x and y
+    if (x > this.x - this.image.width / 2 && x < this.x + this.image.width / 2 && y > this.y - this.image.height / 2 && y < this.y + this.image.height / 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- activities/Tutorial-1-Where's-Sausage-Dog/js/SausageDog.js
+++ activities/Tutorial-1-Where's-Sausage-Dog/js/SausageDog.js
@@ -15,8 +15,11 @@ class SausageDog extends Animal {
   }
 
   mousePressed() {
-    // If mouse is within the dimension of Sausage Dog's img, then img is
-    if (mouseX > this.x - this.image.width / 2 && mouseX < this.x + this.image.width / 2 && mouseY > this.y - this.image.height / 2 && mouseY < this.y + this.image.height / 2) {
+    // If mouse is within the dimension of Sausage Dog's img, then img is found when clicked
+    //if (mouseX > this.x - this.image.width / 2 && mouseX < this.x + this.image.width / 2 && mouseY > this.y - this.image.height / 2 && mouseY < this.y + this.image.height / 2) {
+
+    // Same as previous condition but more neat as previous condition moved to parent class
+    if (this.overlap(mouseX, mouseY)) {
       this.found = true;
     }
   }
