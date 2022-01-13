8aa7cc8b3c9c775a687833a67be0772b0720b278
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-12 21:12:12 -0500

    Create and generate animals

--- activities/Tutorial-1-Where's-Sausage-Dog/js/script.js
+++ activities/Tutorial-1-Where's-Sausage-Dog/js/script.js
@@ -7,26 +7,38 @@ Represent a generic "animal" via classes (using inheritance)
 
 "use strict";
 
+// Declare fixed num for animals and their images
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
-/**
-Description of preload
-*/
+// Declare empty array variables for animals and their images
+let animalImgs = [];
+let animals = [];
+
 function preload() {
-
+  // Create a for loop to load all animal images by using iterator i
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImg = loadImage(`assets/images/animal${i}.png`);
+    // Push here means to load each images declared from the for loop
+    animalImgs.push {
+      animalImg
+    };
+  }
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // Create animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImg = random(animalImgs);
+    let animal = new Animal(x, y, animalImg);
+    animals.push(animal);
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
-
+  g
 }
