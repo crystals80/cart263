55f9f506b00b9c266c6ad47e3d6be8feba2883ec
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-18 14:41:27 -0500

    Review parameters and 1st-class functions

--- inclass-activities/wk1-inclass-activity/js/script.js
+++ inclass-activities/wk1-inclass-activity/js/script.js
@@ -14,6 +14,10 @@ Reviewing lecture 1
 
 "use strict";
 
+function setup() {
+  createCanvas(500, 500);
+}
+
 // (1) CONSTANTS FOR STRINGS
 // Constants are used for repetitive var
 /*const WIDTH = 500;
@@ -21,6 +25,7 @@ const HEIGHT = 500;
 
 // For example, avoid typos & errors in repetitive code
 const IMG_PATH =`assets/images`;
+const NUM_CLOWN_IMGS = 13;
 
 let clownImgs = []
 
@@ -33,10 +38,10 @@ function preload() {
   clownImg[0] = loadImage(`${IMG_PATH}/clown0.png`);
   clownImg[1] = loadImage(`${IMG_PATH}/clown1.png`);
   clownImg[2] = loadImage(`${IMG_PATH}/clown2.png`);
+  // Can also reduce it down like this
+  for (let = i; i < NUM_CLOWN_IMGS; i++) {
+  clownImgs[i] = loadImage(`${IMG_PATH}/clown${i}.png`);
 }
-
-function setup() {
-  createCanvas(500,500);
 }
 
 function draw() {
@@ -47,21 +52,94 @@ function draw() {
 
 // (2) PARAMETERS
 
-function setup() {
-  createCanvas(500, 500);
+//let msg = `NOW YOU SEE ME`;
+//let msgVisible = true;
+
+// Instead of declaring many var separately, declare them ad parameter objects
+/*let msg = {
+  text: `NOW YOU SEE ME`,
+  visible: true
 }
 
 function draw() {
+  background(0);
 
+  // Display msg (or not)
+  if (msg.text === true) {
+    push();
+    fill(255);
+    text(msg, 100, 100); // Text size automatically set at 16 pt
+    pop();
+  }
 }
+
+// You can add function manually in console to hide text
+function hideMsg() {
+  msg.visible = false;
+}*/
 
 
 // (3) FIRST CLASS FUNCTIONS
 
-/*function setup(){
-  createCanvas(500,500);
+/*let msg = {
+  text: `NOW YOU SEE ME`,
+  visible: true,
+  hideDelay: 2000
 }
 
-function draw(){
+function draw() {
+  background(0);
 
-}*/
+  // Display msg (or not)
+  if (msg.text === true) {
+    push();
+    fill(255);
+    text(msg, 100, 100); // Text size automatically set at 16 pt
+    pop();
+  }
+  hideMsg(); // to add 1st-class function but not necessary & basically find var hideMsg and run it
+}
+
+// Hiding msg after a delay time
+function hideMsg() {
+  msg.visible = false;
+}
+
+// Another way to write function but less clean though more true
+// let hideMsg = function () {
+//   msg.visible = false;
+// }
+
+function mousePressed() {
+  //setTimeout(hideMsg, 2000);
+  setTimeout(hideMsg, msg.hideDelay);
+}
+
+// Can also write hideMsg function like this, putting it inside mousePressed()
+// function mousePressed() {
+//   let hideMsg = function() {
+//     msg.visible = false;
+//   }
+//   setTimeout(hideMsg, msg.hideDelay);
+// }
+
+// Can also write anonymous function
+// This is repulsive to look at because not clean and confusing perhaps
+// function mousePressed() {
+//   setTimeout(function (){
+//     msg.visible = false;
+//   }, msg.hideDelay);
+}
+
+// Not a very good example to use () nor practical but good to know
+// function mousePressed() {
+//   //setTimeout(hideMsg, 2000);
+//   // hideMsg = don't get function right away
+//   // getMsgDelay() = get function immediately (as long as the () is there)
+//   setTimeout(hideMsg, getMsgDelay());
+// }
+//
+// function getMsgDelay(){
+//   return msg.hideDelay;
+// }
+*/
