6bc160c7f2b215cdf26fea8209917221b47c94d4
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-18 14:03:05 -0500

    Review constants

--- inclass-activities/wk1-inclass-activity/js/script.js
+++ inclass-activities/wk1-inclass-activity/js/script.js
@@ -1,33 +1,67 @@
 /**
-Title of Project
-Author Name
+In-class Activity - Week 1
+Lam Ky Anh Do
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Reviewing lecture 1
+
+1- Constants for strings? How to know when to do what?
+ - Path names?
+2- Parameters (when to use them? classes vs functions?)
+ - x, y, size?
+3- First class functions
+ - setTimeout()? Maybe showing text for a specific amount of time by switching off text
 */
 
 "use strict";
 
+// (1) CONSTANTS FOR STRINGS
+// Constants are used for repetitive var
+/*const WIDTH = 500;
+const HEIGHT = 500;
 
-/**
-Description of preload
-*/
+// For example, avoid typos & errors in repetitive code
+const IMG_PATH =`assets/images`;
+
+let clownImgs = []
+
 function preload() {
-
+  // So instead of writing the whole image path...
+  //clownImg[0] = loadImage(`assets/images/clown0.png`);
+  //clownImg[1] = loadImage(`assets/images/clown1.png`);
+  //clownImg[2] = loadImage(`assets/images/clown2.png`);
+  // You replace the path by the constant var [ALWAYS inside ${}]
+  clownImg[0] = loadImage(`${IMG_PATH}/clown0.png`);
+  clownImg[1] = loadImage(`${IMG_PATH}/clown1.png`);
+  clownImg[2] = loadImage(`${IMG_PATH}/clown2.png`);
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(500,500);
 }
 
+function draw() {
+  let =clownImg = random(clownImgs);
+  image(clownImg,0,0);
+}*/
 
-/**
-Description of draw()
-*/
+
+// (2) PARAMETERS
+
+function setup() {
+  createCanvas(500, 500);
+}
+
 function draw() {
 
 }
+
+
+// (3) FIRST CLASS FUNCTIONS
+
+/*function setup(){
+  createCanvas(500,500);
+}
+
+function draw(){
+
+}*/
