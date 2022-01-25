d5d77b3d9af3bfa6c5c2becbf23f454f9ef21b03
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-25 17:57:23 -0500

    E2: Set up pointillism background for title screen

--- exercises/Exercise-2-slamina-new-game-plus/js/script.js
+++ exercises/Exercise-2-slamina-new-game-plus/js/script.js
@@ -7,14 +7,41 @@ ResponsiveVoice (https://responsivevoice.org/) & annyang! (https://www.talater.c
 
 "use strict";
 
+// STATE VARIABLE: title, simulation, ending and restart
+let state = "title";
+// IMAGE VARIABLES
+let titleImg;
+
+// TITLE SCREEN VARIABLES
+let width, height;
+
+// SIMULATION VARIABLES
+// Fixed var for animals in an array
 const animals = ["aardvark", "alligator", "alpaca", "antelope", "ape", "armadillo", "baboon", "badger", "bat", "bear", "beaver", "bison", "boar", "buffalo", "bull", "camel", "canary", "capybara", "cat", "chameleon", "cheetah", "chimpanzee", "chinchilla", "chipmunk", "cougar", "cow", "coyote", "crocodile", "crow", "deer", "dingo", "dog", "donkey", "dromedary", "elephant", "elk", "ewe", "ferret", "finch", "fish", "fox", "frog", "gazelle", "gila monster", "giraffe", "gnu", "goat", "gopher", "gorilla", "grizzly bear", "ground hog", "guinea pig", "hamster", "hedgehog", "hippopotamus", "hog", "horse", "hyena", "ibex", "iguana", "impala", "jackal", "jaguar", "kangaroo", "koala", "lamb", "lemur", "leopard", "lion", "lizard", "llama", "lynx", "mandrill", "marmoset", "mink", "mole", "mongoose", "monkey", "moose", "mountain goat", "mouse", "mule", "muskrat", "mustang", "mynah bird", "newt", "ocelot", "opossum", "orangutan", "oryx", "otter", "ox", "panda", "panther", "parakeet", "parrot", "pig", "platypus", "polar bear", "porcupine", "porpoise", "prairie dog", "puma", "rabbit", "raccoon", "ram", "rat", "reindeer", "reptile", "rhinoceros", "salamander", "seal", "sheep", "shrew", "silver fox", "skunk", "sloth", "snake", "squirrel", "tapir", "tiger", "toad", "turtle", "walrus", "warthog", "weasel", "whale", "wildcat", "wolf", "wolverine", "wombat", "woodchuck", "yak", "zebra"]
 
+// Variables stating the current animal or speech recognition answer
 let currentAnimal = ``;
 let currentAnswer = ``;
+
+function preload() {
+  titleImg = loadImage('assets/images/animals.jpg');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  // Set up pointillism background image for title screen
+  push();
+  width = titleImg.width;
+  height = titleImg.height;
+  imageMode(CENTER);
+  titleImg.resize(windowWidth, windowHeight);
+  noStroke();
+  background(255);
+  titleImg.loadPixels();
+  pop();
+
+  // Set up annyang!
   if (annyang) {
     // Let program guess animal by your guess via annyang!'s speech recognition
     let commands = {
@@ -24,17 +51,64 @@ function setup() {
     annyang.addCommands(commands);
     // Start listening
     annyang.start();
-
-    textSize(32);
-    textStyle(BOLD);
-    textAlign(CENTER);
+    //annyang.debug(true); // To debug annyang, assign true/false
+  } else {
+    // annyang doesn't work
+    alert(`Please visit this page on Google Chrome!`)
   }
+
+  // Display speech recognition
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER);
+}
+
+
+function draw() {
+
+  if (state === `title`) {
+    title();
+    pointillismBg(titleImg);
+  } else if (state === `simulation`) {
+    simulation();
+  } else if (state === `ending`) {
+    ending();
+  } else if (state === `restart`) {
+    restart();
+  }
+}
+
+function title() {
 
 }
 
-function draw() {
-  background(0);
+// Trigger ResponsiveVoice to say the reversed animal name
+function mousePressed() {
+  // Assign a random animal name from the animals array to currentAnimal
+  currentAnimal = random(animals);
+  // Say currentAnimal backwards
+  let reverseAnimal = reverseString(currentAnimal);
+  // Use ResponsiveVoice to speak reverse animal names
+  responsiveVoice.speak(reverseAnimal);
+}
 
+function pointillismBg(sourceImage) {
+  for (let i = 0; i < 2; i++) {
+    // floor allows random to give decimal
+    let x = floor(random(sourceImage.width));
+    let y = floor(random(sourceImage.height));
+    let pix = sourceImage.get(x, y);
+    fill(pix, 128);
+    noStroke();
+    ellipse(x, y, 20, 20);
+  }
+}
+
+function simulation() {
+  displayAnswer();
+}
+
+function displayAnswer() {
   // Display whether a guess is right or wrong
   if (currentAnswer === currentAnimal) {
     fill(0, 255, 0);
@@ -42,16 +116,6 @@ function draw() {
     fill(255, 0, 0);
   }
   text(currentAnswer, width / 2, height / 2);
-}
-
-// Trigger ResponsiveVoice to say the reversed animal name
-function mousePressed() {
-  // Assign a random animal name from the animals array to currentAnimal
-  currentAnimal = random(animals);
-  // Reverse currentAnimal using reverseString
-  let reverseAnimal = reverseString(currentAnimal);
-  // Use ResponsiveVoice to speak reverseAnimal
-  responsiveVoice.speak(reverseAnimal);
 }
 
 // Called by annyang!, when it gears a guess
