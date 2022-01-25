6d95ee1f3e6304b9a66a4f7b4e909574f0da42a0
Lam Ky Anh Do <faleanh@hotmail.com>
2022-01-24 23:45:01 -0500

    Added ResponsiveVoice API

---
+++ experiments/voices-experiments/js/1-responsive-voice.js
@@ -0,0 +1,18 @@
+/**
+ResponsiveVoice
+Lam Ky Anh Do
+
+Experimenting with ResponsiveVoice, a speech synthesis library.
+*/
+
+"use strict";
+
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0)
+
+}
---
+++ experiments/voices-experiments/js/2-annyang.js
@@ -0,0 +1,17 @@
+/**
+annyang!
+Lam Ky Anh Do
+
+Experimenting with annyang!, a speech recognition library.
+*/
+
+"use strict";
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0)
+
+}
