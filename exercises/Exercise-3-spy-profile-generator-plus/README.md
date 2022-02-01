16c32a2462abbda8f24965af0e727284a11ffa77
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-01 17:20:43 -0500

    E3: Started Exercise 3 Spy Generator Profile Plus

---
+++ exercises/Exercise-3-spy-profile-generator-plus/README.md
@@ -0,0 +1,3 @@
+# Activity 3: Spy Profile Generator
+
+This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
---
+++ exercises/Exercise-3-spy-profile-generator-plus/js/script.js
@@ -0,0 +1,90 @@
+/**
+Spy Profile Generator
+Lam Ky Anh Do
+
+Generate a randomized spy profile for the user and password protecting it
+*/
+
+"use strict";
+
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`,
+};
+
+let instrumentData, objectData, tarotData;
+
+function preload() {
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+  objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+  tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Pop-up msg as prompt to ask for name
+  // generateSpyProfile();
+  // Have generateSpyProfile() only doesn't save the data BUT adding localStorage will
+  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  // Save and load the generated profile (even after reloading page)
+  if (data !== null) {
+    let password = prompt(`Agent! What is your password?`);
+    if (password === data.password) {
+      spyProfile.name = data.name;
+      spyProfile.alias = data.alias;
+      spyProfile.secretWeapon = data.secretWeapon;
+      spyProfile.password = data.password;
+    }
+  } else {
+    generateSpyProfile();
+  }
+}
+
+// Generate a profile using JSON data
+function generateSpyProfile() {
+  // Type in your name
+  spyProfile.name = prompt(`Agent! What is your name?`);
+  // Generate alias
+  let instrument = random(instrumentData.instruments)
+  spyProfile.alias = `The ${instrument}`;
+  // Generate secret weapon
+  let object = random(objectData.objects)
+  spyProfile.secretWeapon = random(objectData.objects);
+  // Generate password
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
+
+  localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile))
+}
+
+function draw() {
+  background(100, 100, 100);
+
+  // Get the user’s name and display a default profile
+  let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
+
+  Name: ${spyProfile.name}
+  Alias: ${spyProfile.alias}
+  Secret Weapon: ${spyProfile.secretWeapon}
+  Password: ${spyProfile.password}`;
+
+  push();
+  textFont(`Courier,monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  // Instead of having text aligned on after another, set a profile template (see above)
+  // text(spyProfile.name, 100, 100);
+  // text(spyProfile.alias, 100, 200);
+  // text(spyProfile.secretWeapon, 100, 300);
+  // text(spyProfile.password, 100, 400);
+  text(profile, 100, 100);
+  pop();
+}
+
+
+function mousePressed() {
+
+}
