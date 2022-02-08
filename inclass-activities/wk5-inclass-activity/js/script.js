368c1ea8cefe01219a1870d14394c459335fd464
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-08 14:32:41 -0500

    Learned more about HANDPOSE in class

--- inclass-activities/wk5-inclass-activity/js/script.js
+++ inclass-activities/wk5-inclass-activity/js/script.js
@@ -9,10 +9,12 @@ Reviewing lecture 4
 2- ObjectDetector
  - Working with confidence
 3- Blinking
-4-
+4- Machine Learning outside the webcam?
 */
 
 "use strict";
+
+// HANDPOSE - HAND GESTURE (see function highlightHand())
 
 // Current state of program
 let state = `loading`; // loading, running
@@ -98,12 +100,20 @@ function running() {
 Provided with a detected hand it highlights the tip of the index finger
 */
 function highlightHand(hand) {
+  // Change circle colour depending on the distance of the circles
+  let d = dist(indexX, indexY, thumbX, thumbY);
+  if (d < 50) {
+    fill(255, 255, 0);
+  } else {
+    fill(0, 255, 255);
+  }
+
   // Display a circle at the tip of the index finger
   let index = hand.annotations.indexFinger[3];
   let indexX = index[0];
   let indexY = index[1];
   push();
-  fill(255, 255, 0);
+  // fill(255, 255, 0);
   noStroke();
   ellipse(indexX, indexY, 20);
   pop();
@@ -113,9 +123,125 @@ function highlightHand(hand) {
   let thumbX = thumb[0];
   let thumbY = thumb[1];
   push();
-  fill(0, 255, 255);
+  // fill(0, 255, 255);
   noStroke();
   ellipse(thumbX, thumbY, 20);
   pop();
 
+}*/
+
+// OBJECTDETECTOR
+
+// Current state of program
+let state = `loading`; // loading, running
+// User's webcam
+let video;
+// The name of our model
+let modelName = `CocoSsd`;
+// ObjectDetector object (using the name of the model for clarify)
+let cocossd;
+// The current set of predictions made by CocoSsd once it's running
+let predictions = [];
+
+/**
+Starts the webcam and the ObjectDetector
+*/
+function setup() {
+  createCanvas(640, 480);
+
+  // Start webcam and hide the resulting HTML element
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // Start the CocoSsd model and when it's ready start detection
+  // and switch to the running state
+  cocossd = ml5.objectDetector('cocossd', {}, function() {
+    // Ask CocoSsd to start detecting objects, calls gotResults
+    // if it finds something
+    cocossd.detect(video, gotResults);
+    // Switch to the running state
+    state = `running`;
+  });
+}
+
+/**
+Called when CocoSsd has detected at least one object in the video feed
+*/
+function gotResults(err, results) {
+  // If there's an error, report it
+  if (err) {
+    console.error(err);
+  }
+  // Otherwise, save the results into our predictions array
+  else {
+    predictions = results;
+  }
+  // Ask CocoSsd to detect objects again so it's continuous
+  cocossd.detect(video, gotResults);
+}
+
+/**
+Handles the two states of the program: loading, running
+*/
+function draw() {
+  if (state === `loading`) {
+    loading();
+  } else if (state === `running`) {
+    running();
+  }
+}
+
+/**
+Displays a simple loading screen with the loading model's name
+*/
+function loading() {
+  background(255);
+
+  push();
+  textSize(32);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(`Loading ${modelName}...`, width / 2, height / 2);
+  pop();
+}
+
+/**
+Displays the webcam.
+If there are currently objects detected it outlines them and labels them
+with the name and confidence value.
+*/
+function running() {
+  // Display the webcam
+  image(video, 0, 0, width, height);
+
+  // Check if there currently predictions to display
+  if (predictions) {
+    // If so run through the array of predictions
+    for (let i = 0; i < predictions.length; i++) {
+      // Get the object predicted
+      let object = predictions[i];
+      // Highlight it on the canvas
+      highlightObject(object);
+    }
+  }
+}
+
+/**
+Provided with a detected object it draws a box around it and includes its
+label and confidence value
+*/
+function highlightObject(object) {
+  // Display a box around it
+  push();
+  noFill();
+  stroke(255, 255, 0);
+  rect(object.x, object.y, object.width, object.height);
+  pop();
+  // Display the label and confidence in the center of the box
+  push();
+  textSize(18);
+  fill(255, 255, 0);
+  textAlign(CENTER, CENTER);
+  text(`${object.label}, ${object.confidence.toFixed(2)}`, object.x + object.width / 2, object.y + object.height / 2);
+  pop();
 }
