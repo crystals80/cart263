e517ca2799e18d90814c8210139320c69122c1cc
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-08 13:36:55 -0500

    A4: Finished activity 4

--- activities/Activity-4-Bubble-Popper/js/script.js
+++ activities/Activity-4-Bubble-Popper/js/script.js
@@ -2,36 +2,108 @@
 Bubble Popper
 Lam Ky Anh Do
 
-Generate a randomized spy profile for the user and password protecting it
+Pop bubbles with your index finger as a pin
 */
 
 "use strict";
 
-let;
+// Declare global variables
+let handpose;
+let video; // User's webcam
+let predictions = []; // The current set of predictions
+let bubble;
 
-function preload() {
+// const video = document.getElementById('video');
 
-}
 
+// Function setting up canvas and webcam
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // Create a canvas that is best for the ml5.js Handpose feature
+  createCanvas(640, 480);
 
+  // Access user's webcam
+  video = createCapture(VIDEO)
+  // Hide user's webcam till user authorizes webcam to turn on
+  video.hide();
 
+  // Load handpose model
+  handpose = ml5.handpose(video, {
+    // Flip video
+    flipHorizontal: true
+  }, function() {
+    // Check if model is loaded
+    console.log(`Model loaded.`);
+  });
+
+  // Listen for predictions
+  handpose.on('hand', function(results) {
+    console.log(results);
+    predictions = results;
+  });
+
+  // Set up bubble
+  bubble = {
+    x: random(width),
+    y: height,
+    size: 100,
+    vx: 0,
+    vy: -2
+  }
 }
 
+// Function to draw handpose
 function draw() {
-  background(100, 100, 100);
+  background(0);
 
+  // Set coordinates of index finger
+  if (predictions.length > 0) {
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
 
+    // Display index finger as a pin
+    // Draw a line connecting the tip of index finger to its base (Pin body)
+    push();
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+    // Draw a circle at the base of the index finger (Pin head)
+    push();
+    nostroke();
+    fill(255, 255, 0);
+    ellipse(baseX, baseY, 20, 20);
+    pop();
+
+    // Check if bubble pops
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size / 2) {
+      bubble.x = random(width);
+      bubble.y = height;
+    }
+  }
+
+  // Move bubble
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+
+  // Set bubble position (move it into canvas)
+  if (bubble.y < 0) {
+    bubble.x = random(width);
+    bubble.y = height;
+  }
+
+  // Display bubble
   push();
-  textFont(`Courier,monospace`);
-  textSize(24);
-  textAlign(LEFT, TOP);
-  text(profile, 100, 100);
+  fill(0, 100, 200);
+  noStroke();
+  ellipse();
   pop();
-}
-
-
-function mousePressed() {
-
 }
