535bd114ac4c8b60e00c3b5559290d770925b501
Lam Ky Anh Do <65997969+crystals80@users.noreply.github.com>
2022-02-10 11:41:01 -0500

    E4: DEBUGGING - changed speed
    So if I want the bubble to move up, then this.vy should be in the negative numbers

--- exercises/Exercise-4-bubble-popper-plus-plus/js/Bubble.js
+++ exercises/Exercise-4-bubble-popper-plus-plus/js/Bubble.js
@@ -12,7 +12,7 @@ class Bubble {
     this.y = height;
     this.size = 50;
     this.vx = 0;
-    this.vy = random(2,5);
+    this.vy = random(-10,-5);
     this.fill = {
       r: random(255),
       g: random(255),
