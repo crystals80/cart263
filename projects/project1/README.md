feac0dd1f8044612ed16e474c1ae886acf236613
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-15 14:38:10 -0500

    P1: Started A Glimpse of Project SAO
    Finally got inspired and is coding a scene of an anime!

---
+++ projects/project1/README.md
@@ -0,0 +1,3 @@
+# Project 1: A Night at the Movies - A Glimpse of Project SAO
+
+This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
---
+++ projects/project1/js/script.js
@@ -0,0 +1,21 @@
+/**
+Project 1: A Glimpse of Project SAO
+Lam Ky Anh Do
+
+Reanimate an opening scene from Sword Art Online
+*/
+
+"use strict";
+
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(68, 62, 53)
+}
