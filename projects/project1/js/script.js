f4e204a62bd553a35c00b332f0796aa44b96e869
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-21 14:18:29 -0500

    P1: Set up states and commented code

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -2,13 +2,17 @@
 Project 1: A Glimpse of Project SAO
 Lam Ky Anh Do
 
-Reanimate an opening scene from Sword Art Online
+Reanimate an opening scene from Sword Art Online (SAO)
+
+Note: whether I say program, simulation or animation, it refers to the (re)animation of a scene from SAO.
 */
 
 "use strict";
 
+let state = `title`; // Set up state variable for the simulation
+
 let angle = 0; // Set angle in degrees at 0
-let industryLight; // Font var
+let industryLight, philosopher; // Font vars
 // Vars for time shown on clock
 let timer, counter;
 let seconds, minutes;
@@ -16,8 +20,11 @@ let seconds, minutes;
 // Funtion to preload fonts, images and sounds
 function preload() {
   industryLight = loadFont('assets/fonts/Industry-Light.ttf');
+  // playfairDisplay = loadFont('assets/fonts/Playfair-Display-Regular.ttf');
+  philosopher = loadFont('assets/fonts/Philosopher-Regular.ttf');
 }
 
+// Function to set up program
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -25,7 +32,30 @@ function setup() {
   angleMode(DEGREES); // Set rotation angle to degrees instead of radians
 }
 
+// Function to run the program
 function draw() {
+  // Display states
+  if (state === `title`) {
+    title();
+  } else if (state === `openingScene`) {
+    openingScene();
+  }
+  // else if (state === `gearUpScene`) {
+  //   gearUpScene();
+  // } else if (state === `waitingScene`) {
+  //   waitingScene();
+  // }
+}
+
+// Function to set up the title screen before running the simulation
+function title() {
+  background(0);
+
+
+}
+
+// Function to display the 1st scene of the animation
+function openingScene() {
   // Set background for digital clock
   background(181, 230, 235);
 
@@ -42,13 +72,13 @@ function draw() {
   textFont(industryLight);
   fill(0);
   textSize(80);
-  textAlign(RIGHT);
+  textAlign(RIGHT, CENTER);
   text(`2022/11/06 sun`, 50 + width / 2, 125);
   countUp()
   pop();
 }
 
-// Set a count-up timer to display the time of a clock
+// Function to set up a count-up timer to display the time of a clock
 function countUp() {
   // Convert time to seconds as an integer
   timer = int(millis() / 1000);
@@ -63,9 +93,9 @@ function countUp() {
   minutes = floor(counter / 60);
   seconds = counter % 60;
 
-  // Convert your number to a string
+  // Convert the number to a string
   let numberString = `${seconds}`;
-  // Use padStart to add 0s to the front to make sure it's always two characters (digits) long
+  // Use padStart to add the number 0 to the front making sure there are always two digits long when the clock indicates time under 10 seconds
   numberString = numberString.padStart(2, `0`);
 
   // Display count-up timer
