6f17828defcdc09838e9c618171541158dacb52e
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-22 03:42:49 -0500

    P1: Animated 3rd scene
    But still looking for a way to delay time making it stay at 12:59 for a bit before hitting 13:00

--- projects/project1/js/1-main-script.js
+++ projects/project1/js/1-main-script.js
@@ -12,14 +12,14 @@ Note: whether I say program, simulation or animation, it refers to the (re)anima
 // Global vars for any function
 let state = `waitingScene`; // Set up state variable for the simulation
 let nerveGearImg, innerNerveGearImg; // Images vars
-let industryLight, philosopher, latoReg; // Font vars
+let industryLight, industryBold, philosopher, latoReg; // Font vars
 let angle = 0; // Set angle in degrees at 0 ()
 
 // Vars for customizable time-delay timers
 let currentTime = 0; // Timer var at 0 second
 let startTime = 0; // Countdown timer var
 
-// Vars for time shown on clock (openingScene)
+// Vars for time shown on clock (openingScene & waitingScene)
 let timer, counter;
 let seconds, minutes;
 
@@ -31,7 +31,7 @@ let nextScene = 0; // Transition var for chnaging state with time delay
 function preload() {
   // LOAD FONTS
   industryLight = loadFont('assets/fonts/Industry-Light.ttf');
-  // playfairDisplay = loadFont('assets/fonts/Playfair-Display-Regular.ttf');
+  industryBold = loadFont('assets/fonts/Industry-Bold.ttf');
   latoReg = loadFont('assets/fonts/Lato-Regular.ttf');
   philosopher = loadFont('assets/fonts/Philosopher-Regular.ttf');
   // LOAD IMAGES
@@ -59,6 +59,8 @@ function draw() {
     gearUpScene();
   } else if (state === `waitingScene`) {
     waitingScene();
+  } else if (state === `linkStartScene`) {
+    linkStartScene();
   }
 }
 
@@ -100,12 +102,12 @@ function openingScene() {
   textSize(80);
   textAlign(RIGHT, CENTER);
   text(`2022/11/06 sun`, 10 + width / 2, 100);
-  countUp()
+  digitalClockCountUp()
   pop();
 }
 
 // Function to set up a count-up timer to display the time of a clock
-function countUp() {
+function digitalClockCountUp() {
   // Convert time to seconds as an integer
   timer = int(millis() / 1000);
   // Set timer at 55 seconds
@@ -149,6 +151,61 @@ function waitingScene() {
   // Create background with image
   imageMode(CENTER);
   image(innerNerveGearImg, width / 2, height / 2, width, height);
+  timeOnHeadGear();
+  pop();
+
+  /*// After a timed delay and the time becomes 13:00, trigger next state (linkStartScene() aka SCENE 4) with a customized timer (using Ready,Set,Go Method)
+  // SCENE 3 is onscreen and ready to trigger SCENE 4 (READY)
+  if (nextScene === 0) {
+    nextScene = 1;
+    startTime = millis(); // Convert time to seconds
+  }
+  // Once SCENE 3 is onscreen, start countdown timer to trigger SCENE 4 (SET)
+  else if (nextScene === 1) {
+    currentTime = millis() - startTime;
+    // In 2 seconds, trigger SCENE 4 (GO)
+    if (currentTime >= 5000) {
+      // Sub-state is shifting to SCENE 4
+      nextScene = 2;
+    }
+  } else {
+    // SCENE 4 is triggered and displayed
+    waitingScene(); // SCENE 3
+  } // Transition to next state aka linkStartScene()*/
+}
+
+// Function to set up a count-up timer to display the time of a clock
+function timeOnHeadGear() {
+  // Convert time to seconds as an integer
+  timer = int(millis() / 1000);
+  // Set timer at 55 seconds
+  counter = timer + 59;
+
+  if (counter < 59) {
+    // 1 counter = 1 second
+    counter += 1;
+  } else if (counter === 60) {
+    state = `linkStartScene`;
+  }
+
+  // Convert count-up timer into a hour-minute-second format
+  minutes = floor(counter / 60);
+  seconds = counter % 60;
+
+  // Convert the number to a string
+  let numberString = `${seconds}`;
+  // Use padStart to add the number 0 to the front making sure there are always two digits long when the clock indicates time under 10 seconds
+  numberString = numberString.padStart(2, `0`);
+
+  // Display count-up timer
+  push();
+  textFont(industryBold);
+  fill(210);
+  strokeWeight(2.5);
+  stroke(20);
+  textSize(75);
+  textAlign(CENTER, CENTER);
+  text(`1${minutes+2}:${numberString}`, 175, 80);
   pop();
 }
 
