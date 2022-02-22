971820a8078869bc9701329aad66f537d8d1b3da
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-22 14:36:10 -0500

    Completed in-class activity 5

--- inclass-activities/wk5-inclass-activity/js/script.js
+++ inclass-activities/wk5-inclass-activity/js/script.js
@@ -1,251 +1,91 @@
 /**
-In-class Activity - Week 5
+In-class Activity - Week 7
 Lam Ky Anh Do
 
-Reviewing lecture 4
+Reviewing lecture 5
 
-1- Handpose
- - Hand gesture?
-2- ObjectDetector
- - Working with confidence
- - Can you get the data about extremely low-confidence detection?
-3- Blinking (detect dist between top and bottom eyelid)
-4- Machine Learning outside the webcam? (Tensorflow)
+1- How do we iterate via CSS classes in JS?
 */
 
 "use strict";
 
-// HANDPOSE - HAND GESTURE (see function highlightHand())
+// let paras = document.querySelectorAll(.
+//   `paragraph`);
+//
+// let parasArray = Array.from(paras);
+// parasArray.push("New Thing!");
+//
+// // Treat it like a std array
+// for (let i = 0; i < paragraph.length; i++) {
+//   let p = paras[i];
+//   p.innerHTML = `Changed`;
+//   p.style.color = `#ff0000`;
+// }
 
-// Current state of program
-/*let state = `loading`; // loading, running
-// User's webcam
-let video;
-// The name of our model
-let modelName = `Handpose`;
-// Handpose object (using the name of the model for clarity)
-let handpose;
-// The current set of predictions made by Handpose once it's running
-let predictions = [];
+// (1a) Using forEach() to modify CSS classes
 
+// Use forEach with a named function
+// Not guaranteed to work
+// paras.forEach(hoohah);
+//
+// function hoohah(element) {
+//   element.innerHTML = `Hoo-Hah!`;
+// })
 
-// Starts the webcam and the Handpose
+// Use forEach with an anonymous function
+// Works better like this
+// paras.forEach(function(element) {
+//   element.innerHTML = `Hoo-Hah!`;
+// })
+
+// (1c) Using CSS IDs instead of CSS classes
+//
+// paras.forEach(hoohah);
+
+// function hoohah(element) {
+//   element.getElementById(`p2`) = `Hoo-Hah!`;
+// })
+
+// Use forEach to modify/add/remove a specific class
+/*paras.forEach(hoohah);
+
+function hoohah(element) {
+  let = text = element.innerText;
+  if (text.includes(`Lorem`)) {
+    // element.setAttribute(`class`,`highlight`);
+    element.classList.add(`highlight`); // Can add
+    // element.classList.remove(`highlight`); // And can remove
+    // element.classList.add(`highlight otherclass`); // Can have new classes
+  }
+})*/
+
+// (2a) How to animate HTML using p5.js by flickering highlight class
+/*let p1;
+
 function setup() {
-  createCanvas(640, 480);
-
-  // Start webcam and hide the resulting HTML element
-  video = createCapture(VIDEO);
-  video.hide();
-
-  // Start the Handpose model and switch to our running state when it loads
-  handpose = ml5.handpose(video, {
-    flipHorizontal: true
-  }, function() {
-    // Switch to the running state
-    state = `running`;
-  });
-
-  // Listen for prediction events from Handpose and store the results in our
-  // predictions array when they occur
-  handpose.on(`predict`, function(results) {
-    predictions = results;
-  });
+  let p1 = document.getElementById(`p1`);
+  window.requestAnimationFrame(draw);
 }
 
-// Handles the two states of the program: loading, running
-function draw() {
-  if (state === `loading`) {
-    loading();
-  } else if (state === `running`) {
-    running();
-  }
-}
+function draw(){
+  p1.classList.toggle(`highlight`);
 
-// Displays a simple loading screen with the loading model's name
-function loading() {
-  background(255);
-
-  push();
-  textSize(32);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(`Loading ${modelName}...`, width / 2, height / 2);
-  pop();
-}
-
-// Displays the webcam.
-// If there is a hand it outlines it and highlights the tip of the index finger
-function running() {
-  // Display the webcam with reveresd image so it's a mirror
-  let flippedVideo = ml5.flipImage(video);
-  image(flippedVideo, 0, 0, width, height);
-
-  // Check if there currently predictions to display
-  if (predictions.length > 0) {
-    // Technically there will only be ONE because it only detects ONE hand
-    // Get the hand predicted
-    let hand = predictions[0];
-    // Highlight it on the canvas
-    highlightHand(hand);
-  }
-}
-
-// Provided with a detected hand it highlights the tip of the index finger
-function highlightHand(hand) {
-  // Change circle colour depending on the distance of the circles
-  let d = dist(indexX, indexY, thumbX, thumbY);
-  if (d < 50) {
-    fill(255, 255, 0);
-  } else {
-    fill(0, 255, 255);
-  }
-
-  // Display a circle at the tip of the index finger
-  let index = hand.annotations.indexFinger[3];
-  let indexX = index[0];
-  let indexY = index[1];
-  push();
-  // fill(255, 255, 0);
-  noStroke();
-  ellipse(indexX, indexY, 20);
-  pop();
-
-  // Display a circle at the tip of the thumb
-  let thumb = hand.annotations.thumb[3];
-  let thumbX = thumb[0];
-  let thumbY = thumb[1];
-  push();
-  // fill(0, 255, 255);
-  noStroke();
-  ellipse(thumbX, thumbY, 20);
-  pop();
-
+  window.requestAnimationFrame(draw)
 }*/
 
+// (2b) How to animate HTML using p5.js by moving #p1
+let p1;
+let offsetX = 0;
 
-
-
-
-
-
-
-// OBJECTDETECTOR - WORKING WITH CONFIDENCE (see function running())
-
-// Current state of program
-let state = `loading`; // loading, running
-// User's webcam
-let video;
-// The name of our model
-let modelName = `CocoSsd`;
-// ObjectDetector object (using the name of the model for clarify)
-let cocossd;
-// The current set of predictions made by CocoSsd once it's running
-let predictions = [];
-
-/**
-Starts the webcam and the ObjectDetector
-*/
 function setup() {
-  createCanvas(640, 480);
-
-  // Start webcam and hide the resulting HTML element
-  video = createCapture(VIDEO);
-  video.hide();
-
-  // Start the CocoSsd model and when it's ready start detection
-  // and switch to the running state
-  cocossd = ml5.objectDetector('cocossd', {}, function() {
-    // Ask CocoSsd to start detecting objects, calls gotResults
-    // if it finds something
-    cocossd.detect(video, gotResults);
-    // Switch to the running state
-    state = `running`;
-  });
+  let p1 = document.getElementById(`p1`);
+  p1.style.position = `relative`;
+  window.requestAnimationFrame(draw);
 }
 
-/**
-Called when CocoSsd has detected at least one object in the video feed
-*/
-function gotResults(err, results) {
-  // If there's an error, report it
-  if (err) {
-    console.error(err);
-  }
-  // Otherwise, save the results into our predictions array
-  else {
-    predictions = results;
-  }
-  // Ask CocoSsd to detect objects again so it's continuous
-  cocossd.detect(video, gotResults);
-}
-
-/**
-Handles the two states of the program: loading, running
-*/
 function draw() {
-  if (state === `loading`) {
-    loading();
-  } else if (state === `running`) {
-    running();
-  }
-}
+  p1.style.left = `${offsetX}px`;
+  offsetX += 2;
 
-/**
-Displays a simple loading screen with the loading model's name
-*/
-function loading() {
-  background(255);
-
-  push();
-  textSize(32);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(`Loading ${modelName}...`, width / 2, height / 2);
-  pop();
-}
-
-/**
-Displays the webcam.
-If there are currently objects detected it outlines them and labels them
-with the name and confidence value.
-*/
-function running() {
-  // Display the webcam
-  image(video, 0, 0, width, height);
-
-  // Check if there currently predictions to display
-  if (predictions) {
-    // If so run through the array of predictions
-    for (let i = 0; i < predictions.length; i++) {
-      // Get the object predicted
-      let object = predictions[i];
-      // Highlight it on the canvas
-      highlightObject(object);
-
-      // Control confidence level
-      if (object.confidence < 0.5) {
-        console.log(object);
-      }
-    }
-  }
-}
-
-/**
-Provided with a detected object it draws a box around it and includes its
-label and confidence value
-*/
-function highlightObject(object) {
-  // Display a box around it
-  push();
-  noFill();
-  stroke(255, 255, 0);
-  rect(object.x, object.y, object.width, object.height);
-  pop();
-  // Display the label and confidence in the center of the box
-  push();
-  textSize(18);
-  fill(255, 255, 0);
-  textAlign(CENTER, CENTER);
-  text(`${object.label}, ${object.confidence.toFixed(2)}`, object.x + object.width / 2, object.y + object.height / 2);
-  pop();
+  window.requestAnimationFrame(draw)
 }
