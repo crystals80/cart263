43e3a8a0dfae79a8e19883e9ac024da25c46ba58
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-22 16:30:27 -0500

    P1: Animated Scene 3 properly
    Have the state remains at 12:59 for a bit before hitting 13:00

--- projects/project1/js/1-main-script.js
+++ projects/project1/js/1-main-script.js
@@ -10,7 +10,7 @@ Note: whether I say program, simulation or animation, it refers to the (re)anima
 "use strict";
 
 // Global vars for any function
-let state = `waitingScene`; // Set up state variable for the simulation
+let state = `gearUpScene`; // Set up state variable for the simulation
 let nerveGearImg, innerNerveGearImg; // Images vars
 let industryLight, industryBold, philosopher, latoReg; // Font vars
 let angle = 0; // Set angle in degrees at 0 ()
@@ -145,57 +145,90 @@ function gearUpScene() {
   substatesScene2(); // Parts 2-3-4
 } // gearUpScene() as Part 1
 
+let blinkingStartTime = 0;
+let blinkingCurrentTime = 0;
+let blinkingSemiColon = `:`;
+
 // Function to display the 3rd scene of the animation
 function waitingScene() {
-  push();
-  // Create background with image
-  imageMode(CENTER);
-  image(innerNerveGearImg, width / 2, height / 2, width, height);
-  timeOnHeadGear();
-  pop();
+  currentTime = millis() - startTime;
+  if (currentTime <= 5000) {
 
-  /*// After a timed delay and the time becomes 13:00, trigger next state (linkStartScene() aka SCENE 4) with a customized timer (using Ready,Set,Go Method)
-  // SCENE 3 is onscreen and ready to trigger SCENE 4 (READY)
-  if (nextScene === 0) {
-    nextScene = 1;
+    blinkingCurrentTime = Math.floor((millis() - blinkingStartTime) / 500);
+    if (blinkingCurrentTime % 2 == 0) {
+      blinkingSemiColon = `:`;
+    } else {
+      blinkingSemiColon = ` `
+    }
+    console.log(blinkingCurrentTime);
+
+    push();
+    // Create background with image
+    imageMode(CENTER);
+    image(innerNerveGearImg, width / 2, height / 2, width, height);
+    textFont(industryBold);
+    fill(210);
+    strokeWeight(2.5);
+    stroke(20);
+    textSize(75);
+    textAlign(LEFT, CENTER);
+    text(`12${blinkingSemiColon}59`, 90, 80);
+    pop();
+  } else if (currentTime <= 6000) {
+    timeOnHeadGear();
+  } else {
+    state = `linkStartScene`;
+  }
+
+
+  // Trigger a count-up time (timeOnHeadGear()) with a customized timer (using Ready,Set,Go Method)
+  // SCENE 3 is onscreen and ready to trigger timeOnHeadGear() (READY)
+  if (nextScene === 3) {
+    nextScene = 4;
     startTime = millis(); // Convert time to seconds
   }
-  // Once SCENE 3 is onscreen, start countdown timer to trigger SCENE 4 (SET)
-  else if (nextScene === 1) {
+  // Once SCENE 3 is onscreen, start countdown timer to trigger timeOnHeadGear() (SET)
+  else if (nextScene === 4) {
     currentTime = millis() - startTime;
-    // In 2 seconds, trigger SCENE 4 (GO)
+    // In 5 seconds, trigger timeOnHeadGear() (GO)
     if (currentTime >= 5000) {
-      // Sub-state is shifting to SCENE 4
-      nextScene = 2;
+      // Sub-state is shifting to timeOnHeadGear()
+      nextScene = 5;
     }
   } else {
-    // SCENE 4 is triggered and displayed
-    waitingScene(); // SCENE 3
-  } // Transition to next state aka linkStartScene()*/
+    // timeOnHeadGear() is triggered and displayed
+    // timeOnHeadGear();
+  } // Transition to next state aka linkStartScene()
 }
 
 // Function to set up a count-up timer to display the time of a clock
 function timeOnHeadGear() {
+  push();
+  // Create background with image
+  imageMode(CENTER);
+  image(innerNerveGearImg, width / 2, height / 2, width, height);
+  pop();
   // Convert time to seconds as an integer
-  timer = int(millis() / 1000);
-  // Set timer at 55 seconds
-  counter = timer + 59;
-
-  if (counter < 59) {
-    // 1 counter = 1 second
-    counter += 1;
-  } else if (counter === 60) {
-    state = `linkStartScene`;
-  }
-
-  // Convert count-up timer into a hour-minute-second format
-  minutes = floor(counter / 60);
-  seconds = counter % 60;
-
-  // Convert the number to a string
-  let numberString = `${seconds}`;
-  // Use padStart to add the number 0 to the front making sure there are always two digits long when the clock indicates time under 10 seconds
-  numberString = numberString.padStart(2, `0`);
+  // timer = int((millis() - 18000) / 1000);
+  // // Set timer at 55 seconds
+  // counter = timer + 59;
+  //
+  // if (counter < 59) {
+  //   // 1 counter = 1 second
+  //   counter += 1;
+  //   // After a timed delay and the time becomes 13:00, trigger next state (linkStartScene() aka SCENE 4)
+  // } else if (counter === 60) {
+  //   // state = `linkStartScene`;
+  // }
+  //
+  // // Convert count-up timer into a hour-minute-second format
+  // minutes = floor(counter / 60);
+  // seconds = counter % 60;
+  //
+  // // Convert the number to a string
+  // let numberString = `${seconds}`;
+  // // Use padStart to add the number 0 to the front making sure there are always two digits long when the clock indicates time under 10 seconds
+  // numberString = numberString.padStart(2, `0`);
 
   // Display count-up timer
   push();
@@ -204,9 +237,13 @@ function timeOnHeadGear() {
   strokeWeight(2.5);
   stroke(20);
   textSize(75);
-  textAlign(CENTER, CENTER);
-  text(`1${minutes+2}:${numberString}`, 175, 80);
+  textAlign(LEFT, CENTER);
+  text(`13:00`, 90, 80);
   pop();
+}
+
+function linkStartScene() {
+  background(255);
 }
 
 // Function to set up mouse clicks
