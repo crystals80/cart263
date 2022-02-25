be31b6104e6f386930966ba290dac5e24b094ca6
Lam Ky Anh Do <faleanh@hotmail.com>
2022-02-25 16:14:20 -0500

    Learned about HTML & CSS (Review)

---
+++ experiments/5-html-css-dom-experiments/js/script.js
@@ -0,0 +1,23 @@
+/**
+Handpose - ml5.js
+Lam Ky Anh Do
+
+Experimenting with handpose of ml5.js (https://teachablemachine.withgoogle.com/)
+
+********************************
+Handpose Framework
+Pippin Barr
+
+A skeleton framework for using ml5.js's Handpose feature.Includes a loading screen followed by a live webcam feed with a circle drawn at the tip of the user's index finger.
+********************************
+*/
+
+"use strict";
+
+function setup() {
+
+}
+
+function draw() {
+
+}
