fe0af4a8638e47a3987fddfa7196cdc07a4fcd81
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-15 12:27:58 -0400

    Learned about jQuery UI Interactions

--- experiments/7-jquery-ui-experiments/js/jquery-ui-interactions-script.js
+++ experiments/7-jquery-ui-experiments/js/jquery-ui-interactions-script.js
@@ -1,70 +1,59 @@
 /**
-Intro to jQuery
+jQuery UI Interactions
 Lam Ky Anh Do
 
-Experimenting jQuery without p5.js
+Experimenting jQuery UI interactions
 */
 
 "use strict";
 
-// (1) Access ID tag by jQuery
-// let $mainHeading = $(`#main-heading`); // Having the $ before any tag/id/class is to refer to jquery
-// Set CSS text color with jQuery
-// $mainHeading.css(`color`, `#339966`);
+// (1) Make the prisoner draggable
+$(`#prisoner`).draggable();
 
-// (2) Same thing but less lengthy
-// $(`.header`).css(`color`, `#ff0000`); // The common way to access HTML files and CSS features with jQuery
+// (2) Make the prison draggable
+// $(`#prison`).draggable();
 
-// (3) Modify a group of CSS features of a class
-// $(`.header`).css(`color`, `red`);
-// $(`.header`).css(`background-color`, `black`);
-// $(`.header`).css(`font-size`, `3rem`);
-
-// More efficient to write by declaring a var and use it in place of the jQuery version of "getElementByClassName":
-let $headers = $(`.header`);
-
-$headers.css(`color`, `blue`);
-$headers.css(`background-color`, `black`);
-$headers.css(`font-size`, `3rem`);
-
-// (4) Set up multiple CSS features at the same time
-// $(`.header`).css({
-//   "color": `yellow`,
-//   "background-color": `black`,
-//   "font-size": `10rem`
+// (3) Constrain prisoner's movements horizontally
+// $(`#prisoner`).draggable({
+//   axis: `x`
 // });
 
-// (5) Modify text with jQuery
-// $(`#example-span`).text(`a Spaniel`);
+// (4) Constrain prisoner's movements horizontally within the prison
+// $(`#prisoner`).draggable({
+//   axis: `x`,
+//   containment: `#prison`
+// });
 
-// (6) Use var to modify text by reversing it with jQuery
-let spanText = $(`#example-span`).text();
-let reversedSpanText = spanText.split(``).reverse().join(``);
-$(`#example-span`).text(reversedSpanText);
+// (5) Trigger events handlers to make things interesting
+$(`#prisoner`).draggable({
+  containment: `#prison`,
+  // Underline prisoner text when dragged
+  start: function(event, ui) {
+    $(this).css(`text-decoration`, `underline`);
+  },
+  // Revert to normal text when stopped dragging
+  stop: function(event, ui) {
+    $(this).css(`text-decoration`, `none`);
+  }
+});
 
-// (7) Use var to modify HTML element by making the text bold with jQuery
-let spanHTML = $(`#example-span`).html();
-$(`#example-span`).html(`<strong>${spanHTML}</strong>`);
+// (6) Make prisoner undraggable after 5 seconds
+// setTimeout(function() {
+//   $(`#prisoner`).draggable(`disable`)
+// }, 5000)
 
-// (8) Create an editable content using attribute element (attr)
-$(`#main-heading`).attr(`contenteditable`, `true`);
+// (7) Make the prisoner escape/disappear by dropping them in the tunnel
+$(`#escape-tunnel`).droppable({
+  drop: function(event, ui) {
+    // After prisoner is removed the tunnel will automatically center itself
+    $(`#prisoner`).remove();
+  }
+});
 
-// (9) Change text link using jQuery
-let $link = $(`#thicc-link`);
-
-if ($link.attr(`href`) === `https://thi.cc`) {
-  $link.text(`THICC`);
-}
-
-// (10) Create sentences with jQuery
-let $p = $(`<p></p>`);
-// Set text
-$p.text(`Fresh, fresh paragraph!`);
-// Select section to add text
-$(`#second-section`).prepend($p); // At the beginning of the section
-$(`#second-section`).append($p); // At the end of the section
-$(`h2`).after($p); // Immediately after h2 texts
-
-// (11) Remove elements with jQuery
-// $(`#main-heading`).remove(); // Remove main header
-// $(`.header`).remove(); // Remove headers
+// (8) Call droppable jQuery UI function for any DRAGGABLE jQuery object
+$(`#escape-tunnel`).droppable({
+  drop: function(event, ui) {
+    // Note: how we didn’t need to select ui.draggable with $() because, as the documentation says, ui.draggable is already a jQuery object
+    ui.draggable.remove();
+  }
+});
