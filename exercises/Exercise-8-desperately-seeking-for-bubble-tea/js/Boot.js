302193c015aa4e3415f4d7494d9dd32bcae5a5e8
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-21 00:21:28 -0400

    E8: Loaded more images

--- exercises/Exercise-8-desperately-seeking-for-bubble-tea/js/Boot.js
+++ exercises/Exercise-8-desperately-seeking-for-bubble-tea/js/Boot.js
@@ -10,10 +10,20 @@ class Boot extends Phaser.Scene {
 
   // Function to load assets
   preload() {
-    // Load image
-    this.load.image(`avatar`, `assets/images/dizzy-face.png`);
+    // Load avatar images
+    this.load.image(`hungry-avatar`, `assets/images/dizzy-face.png`);
+    this.load.image(`satisfied-avatar`, `assets/images/relieved-face.png`);
+    // Load THE satisfying food
     this.load.image(`bubble-tea`, `assets/images/bubble-tea.png`);
+    this.load.image(`peach`, `assets/images/peach.png`);
+    this.load.image(`strawberry`, `assets/images/strawberry.png`);
+    this.load.image(`sushi`, `assets/images/sushi.png`);
+    this.load.image(`taco`, `assets/images/taco.png`);
+    this.load.image(`fries`, `assets/images/french-fries.png`);
+    // Load the disliked food
     this.load.image(`alcohol`, `assets/images/cocktail.png`);
+    this.load.image(`eggplant`, `assets/images/eggplant.png`);
+    this.load.image(`onion`, `assets/images/onion.png`);
     // Once assets are loaded, launch play scene
     this.load.on(`complete`, () => {
       this.scene.start(`play`);
