05ea94ba13794c401e28d5fd626214b0169eb953
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-21 00:42:18 -0400

    E8: Added more groups of disliked foods
    Need to figure out how to put these groups in an array (if possible)

--- exercises/Exercise-8-desperately-seeking-for-bubble-tea/js/Play.js
+++ exercises/Exercise-8-desperately-seeking-for-bubble-tea/js/Play.js
@@ -9,15 +9,15 @@ class Play extends Phaser.Scene {
   }
 
   create() {
-    // Create avatar emoji
-    this.avatar = this.physics.add.sprite(400, 300, `avatar`);
-    // Set boundary to keep avatar on canvas
-    this.avatar.setCollideWorldBounds(true);
+    // Create hungry avatar emoji
+    this.hungryAvatar = this.physics.add.sprite(400, 300, `hungry-avatar`);
+    // Set boundary to keep hungry avatar on canvas
+    this.hungryAvatar.setCollideWorldBounds(true);
 
     this.happyStomach();
     this.agitatedStomach();
 
-    // Allow user to use keyboard arrows to move avatar emoji
+    // Allow user to use keyboard arrows to move hungry avatar emoji
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
@@ -25,19 +25,19 @@ class Play extends Phaser.Scene {
   happyStomach() {
     // Display bubble tea emoji
     this.bubbleTea = this.physics.add.sprite(0, 0, `bubble-tea`);
-    // Set boundary to keep avatar on canvas
+    // Set boundary to keep hungry avatar on canvas
     this.bubbleTea.setCollideWorldBounds(true);
     // Create random position for bubble tea emoji
     Phaser.Actions.RandomRectangle([this.bubbleTea], this.physics.world.bounds);
-    // Check overlap of avatar emoji and bubble tea emoji
-    this.physics.add.overlap(this.avatar, this.bubbleTea, this.satisfied, null, this);
-    // Check collision of avatar emoji and bubble tea emoji
-    this.physics.add.collider(this.avatar, this.bubbleTea);
+    // Check overlap of hungry avatar emoji and bubble tea emoji
+    this.physics.add.overlap(this.hungryAvatar, this.bubbleTea, this.satisfied, null, this);
+    // Check collision of hungry avatar emoji and bubble tea emoji
+    this.physics.add.collider(this.hungryAvatar, this.bubbleTea);
   }
 
   // Function to call new position of bubble tea emoji
   satisfied(avatar, bubbleTea) {
-    // Generate new position for bubble tea emoji once it is hit by avatar
+    // Generate new position for bubble tea emoji once it is hit by hungry avatar
     Phaser.Actions.RandomRectangle([bubbleTea], this.physics.world.bounds);
   }
 
@@ -46,7 +46,7 @@ class Play extends Phaser.Scene {
     // Create a group of cocktail emojis
     this.alcohol = this.physics.add.group({
       key: 'alcohol',
-      quantity: 20,
+      quantity: 40,
       bounceX: 0.5,
       bounceY: 0.5,
       collideWorldBounds: true,
@@ -54,12 +54,44 @@ class Play extends Phaser.Scene {
       dragY: 50,
     });
 
+    // Create a group of eggplant emojis
+    this.eggplant = this.physics.add.group({
+      key: 'eggplant',
+      quantity: 40,
+      bounceX: 0.5,
+      bounceY: 0.5,
+      collideWorldBounds: true,
+      dragX: 50,
+      dragY: 50,
+    });
+
+    // Create a group of onion emojis
+    this.onion = this.physics.add.group({
+      key: 'onion',
+      quantity: 40,
+      bounceX: 0.5,
+      bounceY: 0.5,
+      collideWorldBounds: true,
+      dragX: 50,
+      dragY: 50,
+    });
+
+    // this.dislikedFood = [this.alcohol, this.eggplant, this.onion];
+
     // Generate alcohol emojis (this.alcohol.getChildren()) at random position within the canvas (this.physics.world.bounds)
     Phaser.Actions.RandomRectangle(this.alcohol.getChildren(), this.physics.world.bounds);
-    // Check collision between avatar emoji and group of cocktail emojis
-    this.physics.add.collider(this.avatar, this.alcohol);
+    // Check collision between hungry avatar emoji and group of cocktail emojis
+    this.physics.add.collider(this.hungryAvatar, this.alcohol);
     // Check collision between each cocktail emoji
     this.physics.add.collider(this.alcohol, this.alcohol);
+
+    // Same for eggplant and onion
+    Phaser.Actions.RandomRectangle(this.eggplant.getChildren(), this.physics.world.bounds);
+    this.physics.add.collider(this.hungryAvatar, this.eggplant);
+    this.physics.add.collider(this.eggplant, this.eggplant);
+    Phaser.Actions.RandomRectangle(this.onion.getChildren(), this.physics.world.bounds);
+    this.physics.add.collider(this.hungryAvatar, this.onion);
+    this.physics.add.collider(this.onion, this.onion);
   }
 
   update() {
@@ -71,23 +103,23 @@ class Play extends Phaser.Scene {
     // Set up cursors movement
     if (this.cursors.left.isDown) {
       // If either left or right is pressed, rotate appropriately
-      this.avatar.setAngularVelocity(-150);
+      this.hungryAvatar.setAngularVelocity(-150);
     } else if (this.cursors.right.isDown) {
-      this.avatar.setAngularVelocity(150);
+      this.hungryAvatar.setAngularVelocity(150);
     }
     // Otherwise stop rotating
     else {
-      this.avatar.setAngularVelocity(0);
+      this.hungryAvatar.setAngularVelocity(0);
     }
 
     // Make avatar rotate as it moves
     if (this.cursors.up.isDown) {
       // If the up key is pressed, accelerate in the current rotation direction
-      this.physics.velocityFromRotation(this.avatar.rotation, 200, this.avatar.body.acceleration);
+      this.physics.velocityFromRotation(this.hungryAvatar.rotation, 200, this.hungryAvatar.body.acceleration);
     }
     // Otherwise, zero the acceleration
     else {
-      this.avatar.setAcceleration(0);
+      this.hungryAvatar.setAcceleration(0);
     }
   }
 }
