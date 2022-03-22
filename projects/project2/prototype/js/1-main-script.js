ea89a8708949efb862f93b1f14db8b098f5f3a25
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-22 16:07:12 -0400

    P2: Setting up the canvas

---
+++ projects/project2/prototype/js/1-main-script.js
@@ -0,0 +1,18 @@
+/**
+Project 2: Escape the Gamingverse
+Lam Ky Anh Do
+
+Create an escape room filled with Genshin impact content, including the puzzle
+
+Note: I will attempt making the puzzle understandable for non- Genshin Impact players.
+*/
+
+"use strict";
+
+function setup() {
+  createCanvas(1000, 600);
+}
+
+function draw() {
+  background(255);
+}
