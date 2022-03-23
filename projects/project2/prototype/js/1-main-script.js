85b3ef79fe112bd9b2416abfeaba21dc23285501
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-23 00:40:21 -0400

    P2: Gave room facets name for prototype
    as the illustrations are not ready

--- projects/project2/prototype/js/1-main-script.js
+++ projects/project2/prototype/js/1-main-script.js
@@ -9,7 +9,14 @@ Note: I will attempt making the puzzle understandable for non- Genshin Impact pl
 
 "use strict";
 
-let state = `lobby`;
+let state = `lobby`; // Declare state(s) var
+
+// Declare fonts vars
+let extraBold;
+
+function preload() {
+  extraBold = loadFont(`assets/fonts/Montserrat-ExtraBold.ttf`);
+}
 
 function setup() {
   createCanvas(1200, 600);
@@ -39,26 +46,63 @@ function draw() {
   }
 }
 
+function textStyling() {
+  textFont(extraBold);
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  fill(`#202020`);
+}
+
 function lobby() {
   background(`#30c990`);
+
+  push();
+  textStyling();
+  text(`LOBBY`, width / 2, height / 2);
+  pop();
 }
 
 function gamingArea() {
   background(`#fd2663`);
+
+  push();
+  textStyling();
+  text(`GAMING AREA`, width / 2, height / 2);
+  pop();
 }
 
 function changingArea() {
   background(`#8203a1`);
+
+  push();
+  textStyling();
+  text(`CHANGING AREA`, width / 2, height / 2);
+  pop();
 }
 
 function restingArea() {
   background(`#1d2563`);
+
+  push();
+  textStyling();
+  text(`RESTING AREA`, width / 2, height / 2);
+  pop();
 }
 
 function ceiling() {
   background(`#b8e12b`);
+
+  push();
+  textStyling();
+  text(`CEILING`, width / 2, height / 2);
+  pop();
 }
 
 function floor() {
   background(`#131635`);
+
+  push();
+  textStyling();
+  text(`FLOOR`, width / 2, height / 2);
+  pop();
 }
