40b92e5f72147a15aacbdc90a277a30f9789d5b2
Lam Ky Anh Do <faleanh@hotmail.com>
2022-03-22 23:17:19 -0400

    P2: Set up facets of room for prototype

--- projects/project2/prototype/js/1-main-script.js
+++ projects/project2/prototype/js/1-main-script.js
@@ -9,10 +9,56 @@ Note: I will attempt making the puzzle understandable for non- Genshin Impact pl
 
 "use strict";
 
+let state = `lobby`;
+
 function setup() {
   createCanvas(1000, 600);
 }
 
 function draw() {
   background(255);
+
+  if (state === `title`) {
+    title();
+  } else if (state === `welcomeMsg`) {
+    welcomeMsg();
+  } else if (state === `lobby`) {
+    lobby();
+  } else if (state === `gamingArea`) {
+    gamingArea();
+  } else if (state === `changingArea`) {
+    changingArea();
+  } else if (state === `restingArea`) {
+    restingArea();
+  } else if (state === `ceiling`) {
+    ceiling();
+  } else if (state === `floor`) {
+    floor();
+  } else if (state === `ending`) {
+    ending();
+  }
+}
+
+function lobby() {
+  background(`#30c990`);
+}
+
+function gamingArea() {
+  background(`#fd2663`);
+}
+
+function changingArea() {
+  background(`#8203a1`);
+}
+
+function restingArea() {
+  background(`#1d2563`);
+}
+
+function ceiling() {
+  background(`#b8e12b`);
+}
+
+function floor() {
+  background(`#131635`);
 }
