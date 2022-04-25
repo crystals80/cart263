5ef575dd5d7337b09a898c29788a0fb4aab541a4
Lam Ky Anh Do <faleanh@hotmail.com>
2022-04-25 16:43:08 -0400

    P2: Fixed show/hide fatui sign pieces
    Show themselves when on the canvas and when their original state will appear; Show themselves when not on canvas but in the inventory area

--- projects/project2/final/js/1-main-script.js
+++ projects/project2/final/js/1-main-script.js
@@ -9,8 +9,6 @@ This JS file is a mix of p5.js, DOM and Jquery.
 */
 
 "use strict";
-let numDropItem = 0;
-let resetDraggableItem = false;
 
 let state = `title`; // Declare state(s) var
 
@@ -21,6 +19,15 @@ let currentArea = 0; // Set the area the player is facing
 let bgMonstadt, bgLiyue, bgInazuma, bgSereniteaPot, bgCelestia, bgEnkanomiya, bgCarpet, fatuiSign, fatuiPuzzlePiece1, fatuiPuzzlePiece2, fatuiPuzzlePiece3, fatuiPuzzlePiece4;
 // Declare fonts vars
 let extraBold;
+
+let foundFatui1 = false;
+let foundFatui2 = false;
+let foundFatui3 = false;
+let foundFatui4 = false;
+
+// Declare vars for draggable function
+let numDropItem = 0;
+let resetDraggableItem = false;
 
 // Function loading fonts, images and sounds
 function preload() {
@@ -44,17 +51,25 @@ function preload() {
 
 // Function configurating the simulation
 function setup() {
+  // Create canvas
   createCanvas(1200, 600);
+
+  // Create a droppable inventory
   $(`#inventory`).droppable({
     classes: {
       "ui-droppable-active": "ui-state-active",
       "ui-droppable-hover": "ui-state-hover"
     },
     drop: function(event, ui) {
-      if (numDropItem < 2) {
-        console.log(ui.draggable[0].id);
+      // If there are 4 fatui sign pieces in the inventory...
+      if (numDropItem < 4) {
+        // ...disable the draggable option for these items
         $("#" + ui.draggable[0].id).draggable("disable");
+        // Check which fatui sign dragged and put in the inventory through console
+        console.log(ui.draggable[0].id);
+        // Count the items in the inventory
         numDropItem++;
+        // Check how many items are dropped in the inventory
         console.log(numDropItem);
       }
       return true;
@@ -67,8 +82,6 @@ function draw() {
   // Set states as scenes for simulation
   if (state === `title`) {
     title();
-    // } else if (state === `welcomeMsg`) {
-    //   welcomeMsg();
   } else if (state === `mondstadt`) {
     mondstadt();
   } else if (state === `liyue`) {
@@ -99,54 +112,93 @@ function title() {
 
 }
 
-
 // Function displaying the lobby of the escape room
 function mondstadt() {
   background(bgMonstadt);
 
-  if (numDropItem === 2 && resetDraggableItem === false) {
-    console.log(`2`);
+  // If there are 4 pieces of fatui sign in the inventory and state is equal to mondstadt...
+  if (numDropItem === 4 && resetDraggableItem === false) {
+    // ...reset draggable to be able to drag items
     resetDraggableItem = true;
-
+    // ...enable draggable to game canvas
     $(`.fatui`).draggable("enable");
+    // ...and make revert valid so that items will not be contained in the inventory
     $(`.fatui`).draggable({
       revert: `valid`,
       containment: `document`,
-
     });
-
-    // });
   };
 }
 
 // Function displaying the gaming area of the escape room
 function liyue() {
   background(bgLiyue);
+
+  // Hide fatui puzzle pieces by default to keep them on their specific state...
+  $(`#fatui1`).hide();
+  $(`#fatui2`).hide();
+  $(`#fatui3`).hide();
+
+  // ...but show them when they are dragged into the inventory area even if their initial state is not displayed
+  if ($(`#fatui1`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui1`).show();
+  }
+  if ($(`#fatui2`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui2`).show();
+  }
+  if ($(`#fatui3`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui3`).show();
+  }
 }
 
 // Function displaying the changing area of the escape room
 function inazuma() {
   background(bgInazuma);
-}
+  // Hide fatui puzzle pieces by default to keep them on their specific state...
+  $(`#fatui2`).hide();
+  $(`#fatui3`).hide();
+  $(`#fatui4`).hide();
 
-// // change the combination in the lock
-// function changeCode() {
-//   // get current combo
-//   let num = $(this).text();
-//   // if reaches 9, reset to 0
-//   if (num === "9") {
-//     $(this).text("0");
-//     // add 1 to the digit
-//   } else {
-//     num = int(num) + 1;
-//     $(this).text(num);
-//   }
-//   // SOUND_COMBO_LOCK.play();
-// }
+  // ...but show them when they are dragged into the inventory area even if their initial state is not displayed
+  if ($(`#fatui2`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui2`).show();
+  }
+  if ($(`#fatui3`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui3`).show();
+  }
+  if ($(`#fatui4`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui4`).show();
+  }
+
+  // Show found fatui sign 1
+  if (foundFatui1 === true) {
+    $(`#fatui1`).show();
+  }
+}
 
 // Function displaying the resting area of the escape room
 function sereniteaPot() {
   background(bgSereniteaPot);
+  // Hide fatui puzzle pieces by default to keep them on their specific state...
+  $(`#fatui1`).hide();
+  $(`#fatui2`).hide();
+  $(`#fatui4`).hide();
+
+  // ...but show them when they are dragged into the inventory area even if their initial state is not displayed
+  if ($(`#fatui1`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui1`).show();
+  }
+  if ($(`#fatui2`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui2`).show();
+  }
+  if ($(`#fatui4`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui4`).show();
+  }
+
+  // Show found fatui sign 3
+  if (foundFatui3 === true) {
+    $(`#fatui3`).show();
+  }
 
   // Display invisible elllipse as button to trigger background change onclick (see mousePressed function)
   push();
@@ -159,11 +211,50 @@ function sereniteaPot() {
 // Function displaying the ceiling of the escape room
 function celestia() {
   background(bgCelestia);
+  // Hide fatui puzzle pieces by default to keep them on their specific state...
+  $(`#fatui1`).hide();
+  $(`#fatui2`).hide();
+  $(`#fatui3`).hide();
+  $(`#fatui4`).hide();
+
+  // ...but show them when they are dragged into the inventory area even if their initial state is not displayed
+  if ($(`#fatui1`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui1`).show();
+  }
+  if ($(`#fatui2`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui2`).show();
+  }
+  if ($(`#fatui3`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui3`).show();
+  }
+  if ($(`#fatui4`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui4`).show();
+  }
 }
 
 // Function displaying the floor of the escape room
 function enkanomiya() {
   background(bgEnkanomiya);
+  // Hide fatui puzzle pieces by default to keep them on their specific state...
+  $(`#fatui1`).hide();
+  $(`#fatui3`).hide();
+  $(`#fatui4`).hide();
+
+  // ...but show them when they are dragged into the inventory area even if their initial state is not displayed
+  if ($(`#fatui1`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui1`).show();
+  }
+  if ($(`#fatui3`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui3`).show();
+  }
+  if ($(`#fatui4`).hasClass(`ui-draggable-disabled`)) {
+    $(`#fatui4`).show();
+  }
+
+  // Show found fatui sign 4
+  if (foundFatui2 === true) {
+    $(`#fatui2`).show();
+  }
 
   // Display invisible circle as button to trigger background change onclick (see mousePressed function)
   push();
@@ -175,16 +266,22 @@ function enkanomiya() {
 
 // Function storing keyboard inputs
 function keyPressed() {
+  // Press "m" to look at the lobby
   if (keyCode === 77) {
     state = `mondstadt`;
+    // Press "l" to look at the gaming area
   } else if (keyCode === 76) {
     state = `liyue`;
+    // Press "i" to look at the changing area
   } else if (keyCode === 73) {
     state = `inazuma`;
+    // Press "s" to look at the bedroom
   } else if (keyCode === 83) {
     state = `sereniteaPot`;
+    // Press "c" to lok at the ceiling
   } else if (keyCode === 67) {
     state = `celestia`;
+    // Press "e" to look at the floor
   } else if (keyCode === 69) {
     state = `enkanomiya`;
   }
@@ -192,16 +289,30 @@ function keyPressed() {
 
 // Function storing mouseclicks inputs
 function mousePressed() {
+  /****** DIGIT LOCK PUZZLE ******/
   // If Inazuma wall is visible on screen...
   if (state === `inazuma` && mouseIsPressed) {
-    console.log(mouseIsPressed);
     // Click on invisible rectangle to...
     $(`#invisible-lock`).click(function() {
-      console.log(`mouseIsPressed`);
       // Display lock
       $(`#lock-puzzle`).show();
     });
+
+    /****** DIGIT LOCK PUZZLE - CLOSE AND CONFIRM BUTTONS ******/
+    // Click button to close the (overlay screen) digit lock puzzle...
+    $(`.close-btn`).click(function() {
+      // ...by hiding it
+      $(`#lock-puzzle`).hide();
+    });
+    // Click button to close the (overlay screen) digit lock puzzle...
+    $(`.confirm-btn`).click(function() {
+      // ...by hiding it
+      $(`#fatui1`).show();
+    });
   }
+
+
+  /****** FIND FATUI SIGN PIECES ******/
   // If Serenitea Pot wall is visible on screen...
   if (state === `sereniteaPot` && mouseIsPressed) {
     // Click on invisible ellipse to...
@@ -224,6 +335,7 @@ function mousePressed() {
     $(`#invisible-carpet`).click(function() {
       // Show Fatui puzzle piece 2
       $(`#fatui2`).show();
+      foundFatui2 = true
     });
   }
 }
